e21f82ee83e99c660825fd420374613f9af00020
Wendong-Fan <w3ndong.fan@gmail.com>
2025-09-29 01:49:28 +0800

    enhance: PR394 Prevent XSS in FolderComponent and block ipcRenderer injection

--- electron/main/fileReader.ts
+++ electron/main/fileReader.ts
@@ -445,10 +445,7 @@ export class FileReader {
 				if (type === 'md') {
 					const content = fs.readFileSync(filePath, 'utf-8')
 					resolve(content)
-				} else if (isShowSourceCode && type === 'html') {
-					const content = fs.readFileSync(filePath, 'utf-8')
-					resolve(content)
-				}else if (type === 'html') {
+				} else if (type === 'html') {
 					const content = fs.readFileSync(filePath, 'utf-8')
 					resolve(content)
 				} else if (["pdf"].includes(type)) {
