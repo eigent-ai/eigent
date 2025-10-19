9724b3714b3a9f912528c4406a06117ae6c3e3a3
a7m-1st <Ahmed.jimi.awelkeir500@gmail.com>
2025-10-19 13:11:07 +0300

    fix: toggle hasMessages for consistancy

--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -214,6 +214,7 @@ const chatStore = (initial?: Partial<ChatStore>) => createStore<ChatStore>()(
 							content: messageContent,
 							attaches: messageAttaches || [],
 						});
+						targetChatStore.getState().setHasMessages(newTaskId, true);
 					}
 				}
 			}
