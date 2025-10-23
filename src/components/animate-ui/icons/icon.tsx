0623577a1dba86f6f6f3c2bfc447486bdf8daefb
puzhen <1303385763@qq.com>
2025-10-23 16:19:59 +0100

    update

--- src/components/animate-ui/icons/icon.tsx
+++ src/components/animate-ui/icons/icon.tsx
@@ -404,7 +404,7 @@ function AnimateIcon({
 
   const content = asChild ? (
     <Slot
-      ref={inViewRef as React.RefCallback<HTMLElement>}
+      ref={inViewRef as any}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onPointerDown={handlePointerDown}
@@ -415,7 +415,7 @@ function AnimateIcon({
     </Slot>
   ) : (
     <motion.span
-      ref={inViewRef as React.RefCallback<HTMLSpanElement>}
+      ref={inViewRef as any}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onPointerDown={handlePointerDown}
